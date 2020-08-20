77278f298737bbc0436e74a50afbb5b816016c0c
Luis Fuentes Auraad <luisfuentes.au@gmail.com>
2020-08-20 14:43:10 -0500

    Refactor: Changed parseStringDotObject arguments

--- string-dot-to-nested-object/index.js
+++ string-dot-to-nested-object/index.js
@@ -1,23 +1,29 @@
-function parseStringDotObject(object) {
-  const keys = Object.keys(object);
-  let result = {};
-
-  for (let i = 0; i < Object.keys(object).length; i++) {
-    parseObject(result, keys[i].split('.'), object[keys[i]]);
-  }
-
-  function parseObject(parentObject, keysArray, value) {
-    if (keysArray.length == 1) {
-      parentObject[keysArray[0]] = value;
-      return parentObject;
-    } else {
-      parentObject[keysArray[0]] = parentObject[keysArray[0]] || {};
-
-      return parseObject(parentObject[keysArray[0]], keysArray.slice(1), value);
+function parseStringDotObject(data) {
+  const mainProcess = (object) => {
+    const keys = Object.keys(object);
+    let result = {};
+    for (let i = 0; i < Object.keys(object).length; i++) {
+      parseObject(result, keys[i].split('.'), object[keys[i]]);
     }
-  }
+    function parseObject(parentObject, keysArray, value) {
+      if (keysArray.length == 1) {
+        parentObject[keysArray[0]] = value;
+        return parentObject;
+      } else {
+        parentObject[keysArray[0]] = parentObject[keysArray[0]] || {};
+        return parseObject(
+          parentObject[keysArray[0]],
+          keysArray.slice(1),
+          value
+        );
+      }
+    }
+    return result;
+  };
 
-  return result;
+  if (data instanceof Array) return data.map((object) => mainProcess(object));
+
+  if (data instanceof Object) return mainProcess(data);
 }
 
 module.exports = {
